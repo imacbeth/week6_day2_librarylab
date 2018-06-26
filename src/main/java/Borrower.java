0b438584ed58c944e23bb441a30618b6526ebaa6
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-26 16:42:45 +0100

    Borrower class set up

---
+++ src/main/java/Borrower.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Borrower {
+
+    ArrayList<Book> collection;
+
+    public Borrower(){
+        this.collection = new ArrayList<>();
+    }
+
+
+}
---
+++ src/test/java/BorrowerTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+
+public class BorrowerTest {
+
+    Book book;
+    Library library;
+    Borrower borrower;
+
+    @Before
+    public void before(){
+        book = new Book();
+        library = new Library();
+        borrower = new Borrower();
+    }
+}
